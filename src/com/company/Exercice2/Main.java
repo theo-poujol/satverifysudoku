7715050d86444f1934fdebef249e538c9872f457
theo-poujol <theo.poujol@etu.univ-amu.fr>
2020-12-23 16:45:39 +0100

    Projets bien finis

--- src/com/company/Exercice2/Main.java
+++ src/com/company/Exercice2/Main.java
@@ -8,8 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-
-        int[][] g = generateGraph(10);
+        int[][] g = generateGraph(30);
 
         int graphG[][] = {{0, 1, 0, 0, 1, 1},  //1
                 {1, 0, 1, 1, 0, 0},  //2
@@ -19,16 +18,16 @@ public class Main {
                 {1, 0, 0, 0, 0, 0}}; //6
 
 
-        long debut = System.nanoTime();
-        reductionIStoSAT(g,5,"ZoneVideSAT.txt");
-        bruteForce("ZoneVideSAT.txt");
-        long fin = System.nanoTime();
-        System.out.println("réduction et brute force en " + (fin-debut)/1_000_000_000F + "s");
+//        long debut = System.nanoTime();
+//        reductionIStoSAT(g,5,"ZoneVideSAT.txt");
+//        bruteForce("ZoneVideSAT.txt");
+//        long fin = System.nanoTime();
+//        System.out.println("réduction et brute force en " + (fin-debut)/1_000_000_000F + "s");
 
-       /* long debut2 = System.nanoTime();
-        reductionIStoSAT(graphG,3,"Result.txt");
+        long debut2 = System.nanoTime();
+        reductionIStoSAT(g,5,"Result.txt");
         //MiniSat Solver
         long fin2 = System.nanoTime();
-        System.out.println("réduction et MiniSat en " + (fin2-debut2)/1_000_000_000F + "s");*/
+        System.out.println("réduction et MiniSat en " + (fin2-debut2)/1_000_000_000F + "s");
     }
 }
